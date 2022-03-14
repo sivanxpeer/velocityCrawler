1a62942cd5e58563c1859a837f7eeed488f70bf8
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-03-14 15:56:05 +0200

    fixing

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,39 +1,32 @@
-<<<<<<< HEAD
-import React from 'react';
+import React, { FC, useState } from 'react';
+import './Navbar.css';
 
-const Navbar = (): JSX.Element => {
-  return <div>Navbar</div>;
-=======
-import React, { FC, useState } from "react";
-import "./Navbar.css";
-
-import imgLogo from "../../assets/images/Velocity.png";
+import imgLogo from '../../assets/images/Velocity.png';
 
 const Navbar: FC = () => {
   const [isClicked, setIsClicked] = useState(false);
 
   return (
     <>
-      <ul className="navbar">
-        <div className="navbar-left">
-          <li className="navbar-item">
-            <i className={isClicked ? "X" : "fa fa-bars"}></i>
+      <ul className='navbar'>
+        <div className='navbar-left'>
+          <li className='navbar-item'>
+            <i className={isClicked ? 'X' : 'fa fa-bars'}></i>
           </li>
-          <li className="navbar-item">
-            <img className="logo" src={imgLogo} />
+          <li className='navbar-item'>
+            <img className='logo' src={imgLogo} />
           </li>
         </div>
-        <div className="navbar-right">
-          <li className="navbar-item">
+        <div className='navbar-right'>
+          <li className='navbar-item'>
             {/* <i className="fa-solid fa-magnifying-glass"></i> */}
             🔎
           </li>
-          <li className="navbar-item login">Login</li>
+          <li className='navbar-item login'>Login</li>
         </div>
       </ul>
     </>
   );
->>>>>>> main
 };
 
 export default Navbar;
