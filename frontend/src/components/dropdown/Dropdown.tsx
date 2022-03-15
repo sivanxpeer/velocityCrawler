2d7204ee1ad7b7f0222e5461bffd099087306c44
sivanxpeer <sivan.x.peer@gmail.com>
2022-03-15 22:04:44 +0200

    conflicts

--- frontend/src/components/dropdown/Dropdown.tsx
+++ frontend/src/components/dropdown/Dropdown.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Dropdown.css';
+import React from 'react'
+import "./Dropdown.css";
 
 type IProps = {
     setIsClicked: (value: boolean) => void ;
@@ -11,28 +11,22 @@ const closeMenu = ()=>{
 }
   return (
     <div className="dropdown">
-      <div className="dropdown-header">
-        <div>
-          <strong>LAB - L</strong>inkedin <strong>A</strong>uto{' '}
-          <strong>B</strong>ot
         <div className="dropdown-header">
             <div><strong>LAB - L</strong>inkedin <strong>A</strong>uto <strong>B</strong>ot</div>
             {/* <div>X</div> */}
             <i className="fa fa-times" aria-hidden="true" onClick={closeMenu}></i>
 
         </div>
-        {/* <div>X</div> */}
-        <i className="fa fa-times" aria-hidden="true"></i>
-      </div>
-      </div>
-      <ul className="dropdown-list">
-        <li className="dropdown-list-item">Home</li>
-        <li className="dropdown-list-item">Scraping Strategies</li>
-        <li className="dropdown-list-item">About</li>
-        <li className="dropdown-list-item">Login</li>
-      </ul>
-    </div>
-  );
-};
+        <ul className="dropdown-list">
+            <li className="dropdown-list-item">Home</li>
+            <li className="dropdown-list-item">Scraping Strategies</li>
+            <li className="dropdown-list-item">About</li>
+            <li className="dropdown-list-item">Login</li>
+        </ul>
 
-export default Dropdown;
+    </div>
+
+  )
+}
+
+export default Dropdown
