229b5e5fc1f5175b278c344069e36420e60826f8
hilamedina <hm18986@gmail.com>
2022-03-16 11:48:10 +0200

    test

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,11 +1,4 @@
 import axios from 'axios';
-// let myURL = 'http://localhost:3000/api';
-// if (process.env.NODE_ENV === 'production') {
-//   myURL = '/';
-// }
-
-// import axios from 'axios';
-
 let url = 'http://localhost:5000/api';
 if (process.env.NODE_ENV === 'production') {
   url = 'https://velocity-crawler-app.herokuapp.com/api';
@@ -13,3 +6,10 @@ if (process.env.NODE_ENV === 'production') {
 export default axios.create({
   baseURL: url,
 });
+
+// let myURL = 'http://localhost:3000/api';
+// if (process.env.NODE_ENV === 'production') {
+//   myURL = '/';
+// }
+
+// import axios from 'axios';
