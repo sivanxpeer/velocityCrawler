14e75ca6bd07fc956f6b37a8de55743062465f04
assafvan <vanunuassaf@gmail.com>
2022-03-16 15:01:56 +0200

    app.js instead index.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,6 +14,13 @@ app.use(express.json({}));
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', startupRouter);
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//   });
+// }
+
 app.listen(PORT, () => console.log("server is running on port "+PORT));
 
 export { app };
