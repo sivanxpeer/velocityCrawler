651ee7958ba995d505cabd9e8d96631eb4a81f30
sivanxpeer <sivan.x.peer@gmail.com>
2022-03-16 13:32:02 +0200

    SC-44 navbar-filter active mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import CardLists from '../src/components/cardsLists/CardsLists';
-// import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import WelcomPage from './components/welcomePage/WelcomPage';
+import React from "react";
+import CardLists from "../src/components/cardsLists/CardsLists";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import WelcomPage from "./components/welcomePage/WelcomPage";
+import NavFilter from "./components/navFilter/NavFilter";
 
 function App() {
   return (
     <div className="App">
-      <WelcomPage></WelcomPage>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomPage/>} />
+      </Routes>
+        {/* <NavFilter></NavFilter> */}
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/navFilter/NavFilter.tsx
+++ frontend/src/components/navFilter/NavFilter.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React ,{useState} from "react";
+import { NavLink,Link } from "react-router-dom";
 import "./NavFilter.css";
 const NavFilter = () => {
+  const [isRelevant,setIsRelevant]=useState(false);
+  const [isLatest,setIsLatest]=useState(false);
+  const [isStared,setIsStared]=useState(false);
+  const handleRelevant=()=>{
+    setIsRelevant(true);
+    setIsLatest(false);
+    setIsStared(false);
+  }
+  const handleLatest=()=>{
+    setIsLatest(true);
+    setIsRelevant(false);
+    setIsStared(false);
+  }
+  const handleStared=()=>{
+    setIsStared(true);
+    setIsLatest(false);
+    setIsRelevant(false);
+  }
   return (
     <nav className="nav-filter">
-        <ul className="nav-filter-list">
-            <li className="nav-filter-item">Relevant</li>
-            <li className="nav-filter-item">Latest</li>
-            <li className="nav-filter-item">Stared</li>
-        </ul>
+      <ul className="nav-filter-list">      
+        <Link to="/" className={isRelevant? "active-nav":"nav-filter-item"} onClick={handleRelevant}>Relevant</Link>
+        <Link to = "/" className={isLatest?"active-nav":"nav-filter-item"} onClick={handleLatest}>Latest</Link>
+        <Link to = "/" className={isStared?"active-nav":"nav-filter-item"} onClick={handleStared}>Stared</Link>
+      </ul>
     </nav>
-  )
+  );
+};
+{
+  /* <Link to="/"><li className={"navbar-sub-item" + (url === "/" ? " active" : "")} >Popular</li></Link> */
 }
 
-export default NavFilter
+export default NavFilter;
