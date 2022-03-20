0b4d6aa96cff00b6452de247a03c00fcc0063862
assafvan <vanunuassaf@gmail.com>
2022-03-20 11:36:32 +0200

    fixing before deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,12 +14,12 @@ app.use(express.json({}));
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', startupRouter);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log("server is running on port "+PORT));
 
